867f5b60af36da5581b18258281c1ab7394e3dd6
Nigel Bowden <wifinigel@gmail.com>
2020-07-23 23:53:02 +0100

    changed func names & added result summary

--- tests/tests_01.sh
+++ tests/tests_01.sh
@@ -11,11 +11,13 @@ COMMENTS="Inital test script"
 SCRIPT_NAME=$(basename $0)
 
 # Tests log file
-LOG_FILE="test_results.log"
+LOG_FILE="${SCRIPT_NAME}_results.log"
 # WLAN Pi status file (hostspot, wiperf etc...)
 STATUS_FILE="/etc/wlanpi-state"
 # initialize test counter
 c=0
+# initialize error counter
+err=0
 
 # root check
 if [[ $EUID -ne 0 ]]; then
@@ -27,19 +29,28 @@ fi
 # Helper functions - see docs at end of file
 ##############################################
 
+summary () {
+  echo ""
+  echo "-----------------------------------"
+  echo " Number tests completed: $c"
+  echo " Errors: $err"
+  echo "-----------------------------------"
+  echo ""
+}
+
 inc () {  c=$((c + 1));  }
 info () {  inc; echo "(info) Test $c : $1" | tee -a $LOG_FILE;  }
-good () {  echo "(pass) $1" | tee -a $LOG_FILE; }
-bad () {  echo "(fail) $1" | tee -a $LOG_FILE; exit 1;  }
+pass () {  echo "(pass) $1" | tee -a $LOG_FILE; }
+fail () {  echo "(fail) $1" | tee -a $LOG_FILE; err=$((err + 1)); summary;  exit 1; }
 comment () {  echo $1 | tee -a $LOG_FILE; }
 
-check () { if [[ $1 ]]; then good; else bad; fi; }
-check_not () { if [[ !  $1 ]]; then good; else bad; fi; }
+check () { if [[ $1 ]]; then pass; else fail; fi; }
+check_not () { if [[ !  $1 ]]; then pass; else fail; fi; }
 
-file_exists () { info "Checking file exists: $1"; if [[ -e $1 ]]; then good; else bad; fi; }
-dir_exists () { info "Checking directory exists: $1"; if [[ -d $1 ]]; then good; else bad; fi; }
-symlink_exists () { info "Checking symlink exists: $1"; if [[ -L $1 ]]; then good; else bad; fi; }
-check_process () { info "Checking process running: $1"; if [[ `pgrep $1` ]]; then good; else bad; fi; }
+file_exists () { info "Checking file exists: $1"; if [[ -e $1 ]]; then pass; else fail; fi; }
+dir_exists () { info "Checking directory exists: $1"; if [[ -d $1 ]]; then pass; else fail; fi; }
+symlink_exists () { info "Checking symlink exists: $1"; if [[ -L $1 ]]; then pass; else fail; fi; }
+check_process () { info "Checking process running: $1"; if [[ `pgrep $1` ]]; then pass; else fail; fi; }
 
 ########################################
 # Test rig overview
@@ -110,14 +121,14 @@ run_tests () {
   info "Checking selection of expected network ports open"
   port_array=(:9600 :9601 :9602 :9603 :9604 :9605 :9606 :9607 :9608)
   for port in "${port_array[@]}"; do
-    if [[ `netstat -a | grep $port` ]]; then good $port; else bad $port; fi
+    if [[ `netstat -a | grep $port` ]]; then pass $port; else fail $port; fi
   done
 
   # check ufw port ranges are configured
   info "Checking expected ufw port ranges are open"
   range_array=(2400:2408/tcp 4800:4808/tcp 9600:9608/tcp 19200:19208/tcp 11520:11528/tcp 2000:2008/tcp)
   for range in "${range_array[@]}"; do
-    if [[ `ufw status | grep $range` ]]; then good $range; else bad $range; fi
+    if [[ `ufw status | grep $range` ]]; then pass $range; else fail $range; fi
   done
 
   # To do:
@@ -126,6 +137,9 @@ run_tests () {
   #     1. DHCP config ?
   # 
   # Others..?
+
+  # Print summary
+  summary
 
   comment ""
   comment "###########################################"
@@ -175,17 +189,17 @@ Test Utility Documentation
 
  inc: increment the test counter (a global var 'c')
  info: write the text in $1 (var passed to function)  and the next test number to stdout & the log file
- good: write a "pass" msg to stdout & the log file, with optional additional msg in $1 (var passed to function)
- bad: write a "fail" msg to stdout & the log file, with optional additional msg in $1 (var passed to function)
+ pass: write a "pass" msg to stdout & the log file, with optional additional msg in $1 (var passed to function)
+ fail: write a "fail" msg to stdout & the log file, with optional additional msg in $1 (var passed to function)
  comment: output text supplied in $1 to std & log file
 
-check: call good() if condition passed is true (can inc option msg via $1), otherwise bad()
-check_not: call good() if condition passed is false (can inc option msg via $1), otherwise bad()
+check: call pass() if condition passed is true (can inc option msg via $1), otherwise fail()
+check_not: call pass() if condition passed is false (can inc option msg via $1), otherwise fail()
 
-file_exists: call good() if file name passed via $1 exists, else call bad()
-dir_exists: call good() if dir name passed via $1 exists, else call bad()
-symlink_exists: call good() if file name passed via $1 is a symlink, else call bad()
-check_process: call good() if process name passed via $1 is running, else call bad()
+file_exists: call pass() if file name passed via $1 exists, else call fail()
+dir_exists: call pass() if dir name passed via $1 exists, else call fail()
+symlink_exists: call pass() if file name passed via $1 is a symlink, else call fail()
+check_process: call pass() if process name passed via $1 is running, else call fail()
 
 
 #################################################################################################################
