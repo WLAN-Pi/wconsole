595b4c9440788d066faa321982d9714a166887e2
Nigel Bowden <wifinigel@gmail.com>
2020-07-24 22:30:00 +0100

    tidied cntr names, added skip, better summary

--- tests/tests_01.sh
+++ tests/tests_01.sh
@@ -15,7 +15,9 @@ LOG_FILE="${SCRIPT_NAME}_results.log"
 # WLAN Pi status file (hostspot, wiperf etc...)
 STATUS_FILE="/etc/wlanpi-state"
 # initialize test counter
-c=0
+test_counter=0
+# test failure number
+failure=0
 # initialize error counter
 err=0
 
@@ -30,27 +32,31 @@ fi
 ##############################################
 
 summary () {
+  if [[ $failure = 0 ]]; then completed=$test_counter; else completed=$failure; fi
   echo ""
   echo "-----------------------------------"
-  echo " Number tests completed: $c"
+  echo " Total tests: $test_counter"
+  echo " Number tests completed: $completed"
   echo " Errors: $err"
   echo "-----------------------------------"
   echo ""
 }
 
-inc () {  c=$((c + 1));  }
-info () {  inc; echo "(info) Test $c : $1" | tee -a $LOG_FILE;  }
-pass () {  echo "(pass) $1" | tee -a $LOG_FILE; }
-fail () {  echo "(fail) $1" | tee -a $LOG_FILE; err=$((err + 1)); summary;  exit 1; }
-comment () {  echo $1 | tee -a $LOG_FILE; }
+inc ()     { test_counter=$((test_counter + 1));  }
+info ()    { inc; echo "(info) Test $test_counter : $1" | tee -a $LOG_FILE;  }
+pass ()    { echo "(pass) $1" | tee -a $LOG_FILE; }
+fail ()    { echo "(fail) $1" | tee -a $LOG_FILE; err=$((err + 1)); failure=$test_counter; }
+skip ()    { echo "(skip) Test skipped due to earlier failure " | tee -a $LOG_FILE; }
+comment () { echo $1 | tee -a $LOG_FILE; }
 
-check () { if [[ $1 ]]; then pass; else fail; fi; }
-check_not () { if [[ !  $1 ]]; then pass; else fail; fi; }
 
-file_exists () { info "Checking file exists: $1"; if [[ -e $1 ]]; then pass; else fail; fi; }
-dir_exists () { info "Checking directory exists: $1"; if [[ -d $1 ]]; then pass; else fail; fi; }
-symlink_exists () { info "Checking symlink exists: $1"; if [[ -L $1 ]]; then pass; else fail; fi; }
-check_process () { info "Checking process running: $1"; if [[ `pgrep $1` ]]; then pass; else fail; fi; }
+check ()     { if [[ ! $failure = 0 ]]; then skip; return; fi;  if [[ $1 ]];   then pass; else fail; fi; }
+check_not () { if [[ ! $failure = 0 ]]; then skip; return; fi;  if [[ ! $1 ]]; then pass; else fail; fi; }
+
+file_exists ()    { info "Checking file exists: $1"; if [[ ! $failure = 0 ]]; then skip; return; fi;  if [[ -e $1 ]]; then pass; else fail; fi; }
+dir_exists ()     { info "Checking directory exists: $1"; if [[ ! $failure = 0 ]]; then skip; return; fi;  if [[ -d $1 ]]; then pass; else fail; fi; }
+symlink_exists () { info "Checking symlink exists: $1"; if [[ ! $failure = 0 ]]; then skip; return; fi;  if [[ -L $1 ]]; then pass; else fail; fi; }
+check_process ()  { info "Checking process running: $1"; if [[ ! $failure = 0 ]]; then skip; return; fi;  if [[ `pgrep $1` ]]; then pass; else fail; fi; }
 
 ########################################
 # Test rig overview
@@ -62,7 +68,7 @@ Test rig description:
 
   1. WLAN Pi running image to be tested
   2. Supported wireless NIC card on one of USB ports
-  3. WLAN Pi is switched in to wconeol mode
+  3. WLAN Pi is switched in to wconsole mode
   4. wconsole config files are default
 
 ======================================================="
@@ -98,6 +104,15 @@ run_tests () {
 
   # check file symbolic links exist
   symlink_exists "/etc/network/interfaces"
+  symlink_exists "/etc/default/isc-dhcp-server"
+  symlink_exists "/etc/dhcp/dhcpd.conf"
+  symlink_exists "/etc/network/interfaces"
+  symlink_exists "/etc/ser2net.conf"
+  symlink_exists "/etc/hostapd.conf"
+  symlink_exists "/etc/sysctl.conf"
+  symlink_exists "/etc/default/ufw"
+  symlink_exists "/etc/ufw/before.rules"
+
 
   # check hostapd running 
   check_process "hostapd"
@@ -119,17 +134,25 @@ run_tests () {
 
   # check expected ports open
   info "Checking selection of expected network ports open"
-  port_array=(:9600 :9601 :9602 :9603 :9604 :9605 :9606 :9607 :9608)
-  for port in "${port_array[@]}"; do
-    if [[ `netstat -a | grep $port` ]]; then pass $port; else fail $port; fi
-  done
+  if [[ $failure = 0 ]]; then 
+    port_array=(:9600 :9601 :9602 :9603 :9604 :9605 :9606 :9607 :9608)
+    for port in "${port_array[@]}"; do
+      if [[ `netstat -a | grep $port` ]]; then pass $port; else fail $port; fi
+    done
+  else
+    skip 
+  fi
 
   # check ufw port ranges are configured
   info "Checking expected ufw port ranges are open"
-  range_array=(2400:2408/tcp 4800:4808/tcp 9600:9608/tcp 19200:19208/tcp 11520:11528/tcp 2000:2008/tcp)
-  for range in "${range_array[@]}"; do
-    if [[ `ufw status | grep $range` ]]; then pass $range; else fail $range; fi
-  done
+  if [[ $failure = 0 ]]; then
+    range_array=(2400:2408/tcp 4800:4808/tcp 9600:9608/tcp 19200:19208/tcp 11520:11528/tcp 2000:2008/tcp)
+    for range in "${range_array[@]}"; do
+      if [[ `ufw status | grep $range` ]]; then pass $range; else fail $range; fi
+    done
+  else
+    skip 
+  fi
 
   # To do:
   #
@@ -165,7 +188,7 @@ case "$1" in
         echo ""
         echo "  $SCRIPT_NAME -v : script version"
         echo "  $SCRIPT_NAME -h : script help"
-        echo "  $SCRIPT_NAME   : run test suite"
+        echo "  $SCRIPT_NAME    : run test suite"
         echo ""
         exit 0
         ;;
@@ -191,15 +214,17 @@ Test Utility Documentation
  info: write the text in $1 (var passed to function)  and the next test number to stdout & the log file
  pass: write a "pass" msg to stdout & the log file, with optional additional msg in $1 (var passed to function)
  fail: write a "fail" msg to stdout & the log file, with optional additional msg in $1 (var passed to function)
+       (sets fail flag to bypass subsequent tests)
+ skip: write a "skip" msg to stdout & the log file
  comment: output text supplied in $1 to std & log file
 
-check: call pass() if condition passed is true (can inc option msg via $1), otherwise fail()
-check_not: call pass() if condition passed is false (can inc option msg via $1), otherwise fail()
+ check: call pass() if condition passed is true (can inc option msg via $1), otherwise fail()
+ check_not: call pass() if condition passed is false (can inc option msg via $1), otherwise fail()
 
-file_exists: call pass() if file name passed via $1 exists, else call fail()
-dir_exists: call pass() if dir name passed via $1 exists, else call fail()
-symlink_exists: call pass() if file name passed via $1 is a symlink, else call fail()
-check_process: call pass() if process name passed via $1 is running, else call fail()
+ file_exists: call pass() if file name passed via $1 exists, else call fail()
+ dir_exists: call pass() if dir name passed via $1 exists, else call fail()
+ symlink_exists: call pass() if file name passed via $1 is a symlink, else call fail()
+ check_process: call pass() if process name passed via $1 is running, else call fail()
 
 
 #################################################################################################################
